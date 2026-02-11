f95fa6b9247075a462d0294d4c6f29c73a0a97fc
Jatin Saraswat <25110cn114@glbitm.ac.in>
2026-02-11 11:55:02 +0530

    Add files via upload

---
+++ CProgram/SwapValues.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter value of a and b: ");
+    scanf("%d %d",&a,&b);
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    printf("New value of a is %d\n",a);
+    printf("New value of b is %d",b);
+
+    return 0;
+
+}
