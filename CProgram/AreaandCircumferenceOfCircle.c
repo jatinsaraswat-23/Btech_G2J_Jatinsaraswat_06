c89b605442d1ae49e23646c5fb5a5028cf6453fc
Jatin Saraswat <25110cn114@glbitm.ac.in>
2026-02-11 11:32:25 +0530

    Add files via upload

---
+++ CProgram/AreaandCircumferenceOfCircle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    float r,A,C;
+    printf("Enter radius of the Circle: ");
+    scanf("%f",&r);
+    A = 3.14*r*r;
+    C = 2*3.14*r;
+    printf("Area = %f\n",A);
+    printf("Circumference = %f",C);
+
+    return 0;
+
+}
