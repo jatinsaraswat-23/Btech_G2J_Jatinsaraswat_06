603f60aa39e9eb6ab65eaabe80d4edbe09ae2831
Jatin Saraswat <25110cn114@glbitm.ac.in>
2026-02-25 12:27:40 +0530

    Add files via upload

---
+++ CProgram/LeapYear.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int Year;
+    printf("Enter Year: ");
+    scanf("%d",&Year);
+    if(Year%400 == 0) {
+        printf("%d is a Leap Year",Year);
+    }
+    else if(Year%100 == 0) {
+        printf("%d is not a Leap Year",Year);
+    }
+    else if(Year%4 == 0) {
+        printf("%d is a Leap Year",Year);
+    }
+    else {
+        printf("%d is not a Leap Year",Year);
+    }
+
+    return 0;
+}
