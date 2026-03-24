e1a67971e9e2052a4a19d05d4928e72c130f8a50
Jatin Saraswat <25110cn114@glbitm.ac.in>
2026-03-24 21:59:19 +0530

    Update Equality.c

--- CProgram/Equality.c
+++ CProgram/Equality.c
@@ -5,9 +5,9 @@ int main()
     printf("Enter First and Second numbers: ");
     scanf("%d %d",&num1,&num2);
     if (num1 == num2) {
-        printf("Both numbers are equal\n");
+        printf("Numbers are equal\n");
     } else {
-        printf("Numbers are not equal\n");
+        printf("Numbers are not equal");
     }
     
     return 0;
