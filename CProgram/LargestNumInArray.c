e1847f09a7524bcb3e629b1465d9947cf3b88494
Jatin Saraswat <25110cn114@glbitm.ac.in>
2026-03-25 10:29:53 +0530

    Add files via upload

---
+++ CProgram/LargestNumInArray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int size, i, largest;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &size);
+    int array[size];
+    printf("Enter %d elements of the array:\n", size);
+    for (i = 0; i < size; i++) {
+        scanf("%d",&array[i]);
+    }
+    largest = array[0];
+    for (i = 1; i < size; i++) {
+        if (largest < array[i]) {
+            largest = array[i];
+        }
+    }
+    printf("The largest element in the array is: %d\n", largest);
+
+    return 0;
+}
